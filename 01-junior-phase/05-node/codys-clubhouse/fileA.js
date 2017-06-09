9d2fd66e217fe3a15e6b448d3aa10fe125f17576
Thomas Kelly <tmkelly28@gmail.com>
2017-06-09 16:39:47 -0400

    add codys clubhouse

---
+++ 01-junior-phase/05-node/codys-clubhouse/fileA.js
@@ -0,0 +1,25 @@
+var clubhousePassword = 12345
+var clubhouseMembers = ['Cody', 'Tom', 'John', '1706']
+
+
+let counter = 0
+
+counter++
+
+module.exports = {
+  clubhousePassword,
+  clubhouseMembers,
+  counter
+}
+
+// module.exports.clubhouseMembers = clubhouseMembers
+// module.exports.clubhousePassword = clubhousePassword
+
+// console.log(window, document)
+//
+//
+// /toys
+//   index.js
+//   chewToy.js
+//   stuffedEggplantEmoji.js
+// index.js
