379f504ac01d5bfe58c13d5c753c3794d7181c1f
Thomas Kelly <tmkelly28@gmail.com>
2017-06-28 11:32:24 -0400

    saving live demo

--- 01-junior-phase/15-tripplanner/persistence/lecture-demo/public/ourCode.js
+++ 01-junior-phase/15-tripplanner/persistence/lecture-demo/public/ourCode.js
@@ -1,3 +1,45 @@
 $(document).ready(() => {
 
+  const renderToys = function (toys) {
+    const $ul = $('#codys-toys')
+    if (Array.isArray(toys)) {
+      const listEls = toys.map(toy => `<li>${toy.name}</li>`)
+      $ul.append(listEls)
+    } else {
+      $ul.append(`<li>${toys.name}</li>`)
+    }
+  }
+
+  const getAllToys = function () {
+    console.log('We are getting those toys!!!')
+    $.ajax({url: '/api/toys', method: 'GET'})
+      .then(renderToys)
+      .catch(error => {
+        console.error(error)
+      })
+
+    console.log('Do we have the toys yet?')
+
+    const $button = $('#add-new-toy-btn')
+    $button.click(function () {
+      const $input = $('#toy-name')
+      const toyName = $input.val()
+      $input.val('')
+
+      $.ajax({
+        url: '/api/toys',
+        method: 'POST',
+        data: {toyName}
+      })
+        .then(renderToys)
+        .catch(error => {
+          console.error(error)
+        })
+    })
+
+  }
+
+
+  getAllToys()
+
 })
--- 01-junior-phase/15-tripplanner/persistence/lecture-demo/server.js
+++ 01-junior-phase/15-tripplanner/persistence/lecture-demo/server.js
@@ -22,6 +22,25 @@ express()
   .use(express.static(path.join(__dirname, 'node_modules')))
   .use(express.static(path.join(__dirname, 'public')))
 
+  .get('/api/toys', function (req, res, next) {
+    Toy.findAll()
+      .then(toys => {
+        // res.setHeader('Content-Type', 'application/json')
+        // res.send(toys)
+        res.json(toys) // Content-Type: 'application/json'
+      })
+      .catch(next)
+  })
+
+  .post('/api/toys', function (req, res, next) {
+    const toyName = req.body.toyName
+    Toy.create({name: toyName})
+      .then(newToy => {
+        res.json(newToy)
+      })
+      .catch(next)
+  })
+
   .get('/', function (req, res, next) {
     res.sendFile(path.join(__dirname, 'index.html'))
   })
