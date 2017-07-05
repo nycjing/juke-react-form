ef373db0967b3afe640ac9a006a312e04573cbeb
Thomas Kelly <tmkelly28@gmail.com>
2017-07-05 15:49:26 -0400

    update

---
+++ 01-junior-phase/19-react-router/demo/app/Dog.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Dog = (props) => {
+
+  return (
+    <h1 className="blue-box">Dog</h1>
+  )
+}
+
+export default Dog;
--- 01-junior-phase/19-react-router/demo/app/index.js
+++ 01-junior-phase/19-react-router/demo/app/index.js
@@ -7,14 +7,71 @@ import './index.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import SpecificDog from './SpecificDog'
+import Dog from './Dog'
 
-const Main = () => (
-  <div>
-    Hello!
+
+const Cat = () => {
+  return (
+    <div className="red-box">
+      <h1>Cat</h1>
+      <Route path="/cat/kitten" component={Kitten} />
+    </div>
+  )
+}
+
+const Kitten = () => {
+  return (
+    <h3 className="green-box">Kitten</h3>
+  )
+}
+
+const Main = () => {
+
+  const favoriteDog = 'cody'
+
+  return (
+    <div>
+      Hello!
+      <ul>
+        <li>
+          <Link to="/dog">Go to Dog</Link>
+        </li>
+        <li>
+          <Link to="/cat">Go to Cat</Link>
+        </li>
+    </ul>
+    <Route exact path="/" component={Cat} />
+    <Route exact path="/dog" component={Dog} />
+    <Route path="/cat" component={Cat} />
+    <Route path="/dog/:puppyName" render={
+      (routeProps) => <SpecificDog {...routeProps} favoriteDog={favoriteDog} />
+    } />
   </div>
-)
+  )
+
+}
 
 ReactDOM.render(
-  <Main />,
+  <Router>
+    <Main />
+  </Router>,
   document.getElementById('app')
 )
+
+
+// Object spread operator
+
+// const obj1 = {
+//   a: 1
+// }
+//
+// const obj2 = {
+//   b: 2,
+//   ...obj1
+// }
+
+
+
+
