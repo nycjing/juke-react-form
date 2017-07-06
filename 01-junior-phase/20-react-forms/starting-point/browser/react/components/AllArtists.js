98b861da06660cde02570f8b1e12eb0fd8d7f3d4
Jing Jia <nycjing@gmail.com>
2017-07-06 18:30:55 -0400

    new playlist state

--- 01-junior-phase/20-react-forms/starting-point/browser/react/components/AllArtists.js
+++ 01-junior-phase/20-react-forms/starting-point/browser/react/components/AllArtists.js
@@ -1,62 +1,68 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import axios from 'axios';
 
 export default class AllArtists extends Component {
 
-  constructor () {
-    super();
-    this.state = {
-      artists: [],
-      inputArtist: ''
-    };
-      this.handleChange = this.handleChange.bind(this)
-  }
+    constructor() {
+        super();
+        this.state = {
+            artists: [],
+            inputArtist: ''
+        };
+        this.handleChange = this.handleChange.bind(this)
+    }
 
-  componentDidMount () {
-    axios.get('/api/artists')
-      .then(res => res.data)
-      .then(artists => this.setState({ artists }));
-  }
+    componentDidMount() {
+        axios.get('/api/artists')
+            .then(res => res.data)
+            .then(artists => this.setState({artists}));
+    }
 
-    handleChange(event){
+    handleChange(event) {
         this.setState({
-            inputArtist : event.target.value
+            inputArtist: event.target.value
         })
     }
-  render () {
 
-     console.log(this.state.artists[0]);
+    render() {
 
-    const artists = this.state.artists.filter(artist=>{
-              return artist.name === this.state.inputArtist
-        });
+        console.log(this.state.artists);
+        let artists = this.state.artists;
+        if (this.state.inputArtist.length > 0) {
+            const artistSelect = this.state.artists.filter(artist => {
+                return artist.name === this.state.inputArtist
+            });
 
-    return (
-        <div>
-        <div>
-        <form className="form-group" style={{marginTop: '20px'}}>
-            <input onChange={this.handleChange}
-                   className="form-control"
-                   placeholder="Enter artist name"
-            />
-        </form>
-        </div>
-        <div>
-        <h3>Artists</h3>
-        <div className="list-group">
-          {
-            artists.map(artist => {
-              return (
-                <div className="list-group-item" key={artist.id}>
-                  <Link to={`/artists/${artist.id}`}>{ artist.name }</Link>
+            console.log(artistSelect);
+            artists = artistSelect
+        }
+
+        return (
+            <div>
+                <div>
+                    <form className="form-group" style={{marginTop: '20px'}}>
+                        <input onChange={this.handleChange}
+                               className="form-control"
+                               placeholder="Enter artist name"
+                        />
+                    </form>
                 </div>
-              );
-            })
-          }
-        </div>
-      </div>
-    </div>
-    );
-  }
+                <div>
+                    <h3>Artists</h3>
+                    <div className="list-group">
+                        {
+                            artists.map(artist => {
+                                return (
+                                    <div className="list-group-item" key={artist.id}>
+                                        <Link to={`/artists/${artist.id}`}>{ artist.name }</Link>
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
---
+++ 01-junior-phase/20-react-forms/starting-point/browser/react/components/NewPlaylist.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+export default function NewPlaylist()  {
+
+    // const NewPlaylist = props.songs;
+
+    return (
+
+        <div className="well">
+            <form className="form-horizontal">
+                <fieldset>
+                    <legend>New Playlist</legend>
+                    <div className="form-group">
+                        <label className="col-xs-2 control-label">Name</label>
+                        <div className="col-xs-10">
+                            <input className="form-control" type="text"/>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-xs-10 col-xs-offset-2">
+                            <button type="submit" className="btn btn-success">Create Playlist</button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    )
+};
+
