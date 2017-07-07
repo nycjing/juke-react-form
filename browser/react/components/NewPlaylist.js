34723c7be7776e85b3c1fae50ab92b8c01a38c34
Jing Jia <nycjing@gmail.com>
2017-07-07 11:53:15 -0400

    button validation

--- browser/react/components/NewPlaylist.js
+++ browser/react/components/NewPlaylist.js
@@ -5,7 +5,8 @@ export default class NewPlaylist extends React.Component {
     constructor() {
         super()
         this.state = {
-            playlistName: ''
+            playlistName: '',
+            fired: false
         }
 
         this.handleChange = this.handleChange.bind(this)
@@ -14,7 +15,8 @@ export default class NewPlaylist extends React.Component {
 
     handleChange(event) {
         this.setState({
-            playlistName: event.target.value
+            playlistName: event.target.value,
+            fired: true
         })
         console.log(event.target.value)
     }
@@ -27,27 +29,33 @@ export default class NewPlaylist extends React.Component {
     addNewFeed(text){
         console.log('New feed', text)
         this.setState({
-            playlistName: ''
+            playlistName: '',
+            fired: true
         })
     }
 
     render() {
-
+    console.log(!this.state.fired)
         return (
+
 
             <div className="well">
                 <form className="form-horizontal" onSubmit={this.handleSubmit}>
                     <fieldset>
                         <legend>New Playlist</legend>
+                        {this.state.fired && !this.state.playlistName.length || this.state.playlistName.length>16 ?
+                              <div className="alert alert-warning">Please enter a valid playlist name</div>:null}
                         <div className="form-group">
                             <label className="col-xs-2 control-label">Name</label>
                             <div className="col-xs-10">
-                                <input onChange={this.handleChange} value={this.state.playlistName}  className="form-control" type="text"/>
+                                <input onChange={this.handleChange} value={this.state.playlistName}
+                                       className="form-control" type="text"/>
                             </div>
                         </div>
                         <div className="form-group">
                             <div className="col-xs-10 col-xs-offset-2">
-                                <button type="submit" className="btn btn-success">Create Playlist</button>
+                                <button disabled={this.state.playlistName.length>16 || !this.state.playlistName}
+                                        type="submit" className="btn btn-success">Create Playlist</button>
                             </div>
                         </div>
                     </fieldset>
